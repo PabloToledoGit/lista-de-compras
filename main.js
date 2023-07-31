5f4593a181ca133b12185005aaa421fef2e957d5
PabloToledoGit <pablotoledoemail@gmail.com>
2023-07-31 15:19:33 -0300

    estudando

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+const listaDeItens = []
+
+cliente = {
+    nome: 'Mariana', 
+    idade: 35,
+    genero: 'Feminino' 
+}
+
+cliente['estado'] = 'pernambuco';
+
+alert('Olá, meu nome é ' + cliente.nome + ' ' + 'e moro no estado de ' + cliente.estado);
